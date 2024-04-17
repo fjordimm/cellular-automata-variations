9fa7eed5fc80dcf2d553eaf83b80242b9fdac0dc
fjordimm <fjordimm@gmail.com>
2024-04-16 21:56:22 -0700

    added another ruleset

--- scripts/userCellTypes.js
+++ scripts/userCellTypes.js
@@ -1,23 +1,30 @@
 
 userCellTypesList = [
 	["Conway", "#F0C800", function(grid, c, r, cellNone, cellMe, cellOthers) {
-		// return grid.get(c, r) == cellMe || grid.get(c - 1, r) == cellMe || grid.get(c + 1, r) == cellMe;
-
 		let isMe = grid.get(c, r) == cellMe;
 
+		function isCool(x) {
+			return x == cellMe || x == cellOthers.getCellTypeId("Schmonway");
+		}
+
 		let count = 0;
-		if (grid.get(c - 1, r - 1) == cellMe) count++;
-		if (grid.get(c + 0, r - 1) == cellMe) count++;
-		if (grid.get(c + 1, r - 1) == cellMe) count++;
-		if (grid.get(c - 1, r + 0) == cellMe) count++;
-		if (grid.get(c + 1, r + 0) == cellMe) count++;
-		if (grid.get(c - 1, r + 1) == cellMe) count++;
-		if (grid.get(c + 0, r + 1) == cellMe) count++;
-		if (grid.get(c + 1, r + 1) == cellMe) count++;
+		if (isCool(grid.get(c - 1, r - 1))) count++;
+		if (isCool(grid.get(c + 0, r - 1))) count++;
+		if (isCool(grid.get(c + 1, r - 1))) count++;
+		if (isCool(grid.get(c - 1, r + 0))) count++;
+		if (isCool(grid.get(c + 1, r + 0))) count++;
+		if (isCool(grid.get(c - 1, r + 1))) count++;
+		if (isCool(grid.get(c + 0, r + 1))) count++;
+		if (isCool(grid.get(c + 1, r + 1))) count++;
 
 		if (count <= 1) return false;
 		if (count == 2) return isMe;
 		if (count == 3) return true;
 		if (count >= 4) return false;
+	}],
+	["Schmonway", "#E03050", function(grid, c, r, cellNone, cellMe, cellOthers) {
+		let isMe = grid.get(c, r) == cellMe;
+
+		return isMe;
 	}]
 ];
